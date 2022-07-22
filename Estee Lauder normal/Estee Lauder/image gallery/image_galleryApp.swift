ddaf6e9010cf1e6958458ffe7671b7a1205b42d5
Alaliya <95351383+Alaliya@users.noreply.github.com>
2022-07-22 06:20:39 +0600

    Add files via upload

---
+++ Estee Lauder normal/Estee Lauder/image gallery/image_galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_galleryApp.swift
+//  image gallery
+//
+//  Created by Алия Асхаткызы on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct image_galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Estee Lauder normal/Estee Lauder/image gallery/was Estee Lauder app/Estee Lauder app/views/onboarding/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  Estee Lauder app
+//
+//  Created by Алия Асхаткызы on 22.07.2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
